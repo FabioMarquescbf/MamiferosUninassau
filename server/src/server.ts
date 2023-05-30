a772c24e391859addf4708c363efcff6820d4fbb
Fábio Marques <108775609+FabioMarquescbf@users.noreply.github.com>
2023-05-29 23:30:31 -0300

    commit, dotenv e cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,23 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const server = fastify();
+
+
+
+server.register(cors, {
+    
+});
+
 
 // Rota de Teste (Respondendo com Texto regular)
 server.get('/', (request, reply) => { //request pedido, reply retorno
     return "Servidor Exemplo On Line..."
-})
+});
+
+const port: any = process.env.PORT;
 
 server.listen({ port: 3000}, (error, address) => {
     if (error) {
